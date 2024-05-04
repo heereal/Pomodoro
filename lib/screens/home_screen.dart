0c76b4f11449884a0083c0f340aee60d0775d0c7
heeryoung <winterbooks@naver.com>
2024-05-04 22:11:32 +0900

    design: home screen UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,31 +12,71 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
       body: Column(
         children: [
           // UI를 비율에 기반해서 유연하게 만들어 주는 위젯
           Flexible(
             flex: 1,
             child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.green,
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                '25:00',
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
             ),
           ),
           Flexible(
-            flex: 2,
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.orange,
+            flex: 3,
+            child: Center(
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.play_circle_outline),
+                iconSize: 120,
+                color: Theme.of(context).cardColor,
               ),
             ),
           ),
           Flexible(
             flex: 1,
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.blue,
-              ),
+            child: Row(
+              children: [
+                // width 100% 만들기 위해 사용한 위젯
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Pomodoros',
+                          style: TextStyle(
+                            color:
+                                Theme.of(context).textTheme.displayLarge!.color,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(
+                          '0',
+                          style: TextStyle(
+                            color:
+                                Theme.of(context).textTheme.displayLarge!.color,
+                            fontSize: 50,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
